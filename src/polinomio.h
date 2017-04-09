15f606c7c3c222a256703278420167f6916c8637
rodtheo <theodoro.biotec@gmail.com>
2017-04-09 17:58:17 -0300

    Update polinomio.h

--- src/polinomio.h
+++ src/polinomio.h
@@ -1,5 +1,10 @@
+// Aluno: Rodrigo Theodoro Rocha
+// Matricula: 15/0157967
+// Projeto: trab1
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 typedef struct nodo{
   float coeficiente;
@@ -13,11 +18,6 @@ typedef struct polinomio{
 
 // Assinaturas das funcoes
 
-/**
- * Funcao retorna um nodo do polinomio inicializado
- * com zero e ponteiro NULL
- * @return nodo
- */
 p_nodo *getNodo(float coef, int expo);
 
 l_polinomio *getPolinomio();
@@ -26,12 +26,15 @@ void inserir_nodo (l_polinomio *pol, float coef, int expo);
 
 void imprime_polinomio (l_polinomio *pol, char n);
 
-void calcula_polinomio (l_polinomio *pol, float x);
+float calcula_polinomio (l_polinomio *pol, float x);
 
 void inserir_nodo (l_polinomio *pol, float coef, int expo);
 
 p_nodo *busca (l_polinomio *pol, int expo);
 
+void liberaMemoria (l_polinomio *pol);
+
+// Fim das assinaturas das funcoes
 
 /**
  * Funcao retorna um nodo do polinomio inicializado
@@ -48,28 +51,49 @@ p_nodo *getNodo(float coef, int expo) {
   return nodo;
 }
 
+/**
+ * Funcao retorna o polinomio 
+ * @return polinomio
+ */
 l_polinomio *getPolinomio(){
   l_polinomio *pol = (l_polinomio*)malloc(sizeof(l_polinomio));
-
   pol->primeiro = NULL;
 
   return pol;
 }
 
+/**
+ * Libera a memoria alocada para o polinomio pol
+ */
+void liberaMemoria (l_polinomio *pol) {
+    p_nodo *ptr;
+    
+    pol->primeiro;
+    while (ptr != NULL) {
+        free (ptr);
+    }
+    free (pol);
+}
+
+/**
+ * Insere um nodo com coeficiente e expoente especificados
+ * no polinomio pol
+ */
 void inserir_nodo (l_polinomio *pol, float coef, int expo){
   p_nodo *nodo = getNodo(coef, expo);
   p_nodo *ptr;
 
   // If the poli is empty then poli pointer to nodo
   if (pol->primeiro == NULL) {
-    pol->primeiro = nodo;
     printf("not null");
-    } else { // If not add nodo in the begin of poli
+    pol->primeiro = nodo;
+    }
+  else { // If not add nodo in the begin of poli
     // we pointer the next of nodo to the old first-nodo in poli
     // so the old first-nodo becomes the second element of the poli
     // and the inserted nodo the first element (pointing the poli structure to it)
     ptr = busca(pol, expo);
-    printf("ptr: %i", ptr->expoente);
+    // printf("ptr: %i", ptr->expoente);
     if ( ptr != NULL) {
       ptr->coeficiente = ptr->coeficiente + coef;
     } else {
@@ -78,29 +102,58 @@ void inserir_nodo (l_polinomio *pol, float coef, int expo){
   }
 }
 
-p_nodo* busca (l_polinomio *pol, int expo){
-    p_nodo* ptr;
+/**
+ * Esta funcao busca por um nodo com expoente expo no polinomio pol
+ * Retorna o endereco do nodo, se encontrado, ou NULL caso contrario
+ */
+p_nodo *busca (l_polinomio *pol, int expo){
+    p_nodo *ptr;
     ptr = pol->primeiro;
-    while (ptr != NULL && ptr->expoente != expo){
+    if (ptr != NULL){
+      printf("expoente: %i", ptr->expoente);}
+    else {
+      printf("expoente: NULL");
+    }
+    while ((ptr != NULL) && (ptr->expoente != expo)){
       ptr = ptr->proximo;
     }
     return ptr;
   }
 
+/**
+ * Imprime o polinomio
+ */
 void imprime_polinomio (l_polinomio *pol, char n) {
     p_nodo *ptr;
 
     ptr = pol->primeiro;
-    if (ptr == NULL){
-      printf("p(X) = ");
-    }
+      printf("%c(X) = ", n);
     while (ptr != NULL) {
       if (ptr->coeficiente < 0){
-        printf("-%f*X^%i", ptr->coeficiente, ptr->expoente);
-      } else {
-        printf("-%f*X^%i", ptr->coeficiente, ptr->expoente);
-      }
+          if (ptr->expoente == 0){
+            printf("-%.2f", fabs(ptr->coeficiente));
+          } else {
+        printf("-%.2f*X^%i", fabs(ptr->coeficiente), ptr->expoente);}
+        } else {
+           if (ptr->expoente == 0){
+            printf("-%.2f", fabs(ptr->coeficiente));
+          } else {
+        printf("+%.2f*X^%i", fabs(ptr->coeficiente), ptr->expoente);}
+        }
       ptr = ptr->proximo;
     }
     printf("\n\n");
+}
+
+/**
+ * Calcula o valor do polinomio para x
+ * Retorna um float com o resultado
+ */
+float calcula_polinomio (l_polinomio *pol, float x) {
+    p_nodo *ptr;
+    float res = 0;
+    for (ptr=pol->primeiro; ptr != NULL; ptr=ptr->proximo) {
+        res = (ptr->coeficiente)*pow(x, ptr->expoente);
+    }
+    return res;
 }
