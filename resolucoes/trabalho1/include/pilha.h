52c9745709c88876954682919379f71fec8e5ff7
Rodrigo Theodoro <theodoro.biotec@gmail.com>
2017-04-26 13:33:07 -0300

    -make test working in trabalho1

--- resolucoes/trabalho1/include/pilha.h
+++ resolucoes/trabalho1/include/pilha.h
@@ -13,17 +13,36 @@
 #define STACKSIZE 1001
 #define NLINES 6
 #define SIZEWORD 1000
+#define TINT    2
+#define TCHAR   3
+#define TFLOAT  4
+
 
 /**
- * @brief Composição da struct pilha.
+ * @brief Elementos que constituem a pilha genérica.
+ *
+ * Os elementos podem ser do tipo int, char ou float.
+ */
+typedef struct elementPilha {
+  int etype;
+  union {
+    char *cval; /* Ponteiro para uma string */
+    int ival;
+    float fval;
+  } element;
+} t_elementPilha;
+
+/**
+ * @brief Composição da struct pilha Genérica.
  *
  * A pilha é uma struct composta por um vetor de tamanho definido pela macro
  *  STACKSIZE e um inteiro que indicará o topo da pilha.
  */
 typedef struct pilha {
-  char stack[STACKSIZE];
+  struct elementPilha stack[STACKSIZE];
   int p;
 } t_pilha;
+
 
 /**
  * Função para alocar a memória da pilha e setar o topo igual a zero (zero
@@ -36,7 +55,7 @@ t_pilha* get_pilha ();
  * Imprime o índice p do topo da pilha, isto é, a altura da pilha.
  * @param pilha Endereço da pilha criada com get_pilha().
  */
-void top (t_pilha *pilha);
+int top (t_pilha *pilha);
 
 /**
  * Insere um elemento do tipo char na pilha.
@@ -46,6 +65,13 @@ void top (t_pilha *pilha);
  */
 int push (t_pilha *pilha, char valor);
 
+
+int push_float (t_pilha *pilha, float valor);
+
+int push_int (t_pilha *pilha, int valor);
+
+int push_char (t_pilha *pilha, char *pvalor);
+
 /**
  * Retira o último elemento da @p pilha e armazena-o no ponteiro de char @p el.
  * @param  pilha Endereço da pilha criada com #get_pilha.
@@ -53,7 +79,7 @@ int push (t_pilha *pilha, char valor);
  * @return       Retorna -1 se a pilha está vazia ou 1 se a função foi executada
  * com sucesso.
  */
-int pop (t_pilha *pilha, char *el);
+int pop (t_pilha *pilha, t_elementPilha *el);
 
 /**
  * Avalia se a pilha está vazia.
--- resolucoes/trabalho1/tests/check_pilha.c
+++ resolucoes/trabalho1/tests/check_pilha.c
@@ -5,22 +5,65 @@
 START_TEST(test_behaviour_stack)
 {
   t_pilha *pilha = get_pilha();
-  char elPushed='a';
-  char *elPoped;
+  char elCharPushed='a';
+  char elStringPushed[3] = "abc";
+  int elIntPushed=100;
+  float elFloatPushed=1.54;
+  t_elementPilha elPoped;
 
   // Checar se a pilha está vazia, deve retornar verdadeiro
   ck_assert_int_eq(empty (pilha), 1);
   // Inserimos o elemento elPushed na pilha
-  push (pilha, elPushed);
+  push_char (pilha, &elCharPushed);
+
+  push_int (pilha, elIntPushed);
+
+  push_float (pilha, elFloatPushed);
+
+  ck_assert_int_eq(top(pilha), 102932);
   // Retiramos o elemento da pilha
-  pop (pilha, elPoped);
+  pop (pilha, &elPoped);
+  ck_assert_int_eq(top(pilha), 1);
+  ck_assert_float_eq(elPoped.element.fval, 1.54);
+
+  pop (pilha, &elPoped);
+  ck_assert_int_eq(top(pilha), 0);
+  ck_assert_int_eq(elPoped.element.ival, 100);
+
   // Check if the element popped is 'a'
-  ck_assert_str_eq(elPoped, "a");
+  pop (pilha, &elPoped);
+  ck_assert_int_eq(empty(pilha), 1);
+  ck_assert_str_eq(elPoped.element.cval, "a");
+
   // We free the stack's allocated memory
   free_pilha(pilha);
 }
 END_TEST
 
+Suite * pilha_suite (void){
+  Suite *s;
+  TCase *tc_core;
+
+  s = suite_create ("Pilha");
+
+  tc_core = tcase_create ("Core");
+
+  tcase_add_test (tc_core, test_behaviour_stack);
+  suite_add_tcase (s, tc_core);
+
+  return s;
+}
+
 int main(void){
-  return 0;
+  int number_failed;
+  Suite *s;
+  SRunner *sr;
+
+  s = pilha_suite ();
+  sr = srunner_create (s);
+
+  srunner_run_all (sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed (sr);
+  srunner_free(sr);
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
