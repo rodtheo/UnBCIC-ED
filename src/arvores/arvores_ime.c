e17102ed01944ee3846884e732fa8eae30a6529f
rodtheo <theodoro.biotec@gmail.com>
2017-06-07 17:34:30 -0300

    adicionado exercicios ime-arvores

---
+++ src/arvores/arvores_ime.c
@@ -0,0 +1,167 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct reg {
+  int conteudo;
+  struct reg* esq;
+  struct reg* dir;
+} noh;
+
+typedef noh* arvore;
+
+noh* cria_no (int dado) {
+  noh* resultado = (noh*)malloc(sizeof(noh));
+
+  resultado->conteudo = dado;
+  resultado->esq = NULL;
+  resultado->dir = NULL;
+
+  return resultado;
+}
+
+noh* inserir (noh* raiz, int dado) {
+  if (raiz == NULL){
+    return cria_no (dado);
+  }
+  else {
+      if (raiz->conteudo > dado)
+        raiz->esq = inserir (raiz->esq, dado);
+      else if (raiz->conteudo < dado)
+        raiz->dir = inserir (raiz->dir, dado);
+    }
+
+  return raiz;
+}
+
+void em_ordem (arvore raiz) {
+  if (raiz != NULL) {
+    em_ordem (raiz->esq);
+    printf ("%d\n",raiz->conteudo);
+    em_ordem (raiz->dir);
+  }
+}
+
+// Recebe a raiz r de uma árvore binária e
+// imprime os conteúdos dos seus nós
+// em ordem e-r-d.
+
+void em_ordem_i (noh* raiz) {
+  noh *x, *pilha[101];
+  int t=0;
+  pilha[t++] = raiz;
+  while (1) {
+    // pilha[0..t-1] é uma pilha de nós
+    x = pilha[t-1];
+    if (x != NULL)
+      pilha[t++] = x->esq;
+    else {
+      if (t==0) break;
+      x = pilha[--t];
+      printf ("%d\n", x->conteudo);
+      pilha[t++] = x->dir;
+    }
+  }
+}
+
+void count_nodes (noh* raiz, int *n) {
+  if (raiz != NULL){
+    count_nodes (raiz->esq, n);
+    *n = *n + 1;
+    count_nodes (raiz->dir, n);
+  }
+}
+
+void busca_folha (noh* raiz) {
+  if (raiz != NULL) {
+
+    if (raiz->dir == NULL && raiz->esq == NULL){
+    printf("%d\n", raiz->conteudo);
+    return;}
+
+    busca_folha (raiz->dir);
+    busca_folha (raiz->esq);
+  }
+  // return busca_folha (raiz->dir);
+}
+
+void busca_conteudo (noh* raiz, int dado) {
+  if (raiz != NULL) {
+
+    if (raiz->conteudo == dado) {
+      printf("Achado=%d\n", raiz->conteudo);
+      return;
+    }
+
+    busca_conteudo (raiz->dir, dado);
+    busca_conteudo (raiz->esq, dado);
+}}
+
+// Breadth-first search
+
+void em_largura(t_no* raiz){
+    t_fila* fila = getFila(20);
+    t_no* no     = NULL;
+
+    if(raiz == NULL)
+        return;
+
+    inserirFila(fila, raiz);
+
+    while( !filaVazia(fila) ){
+        no = removerFila(fila);
+
+        printf("%d\n", no->dado);
+
+        if(no->esq != NULL)
+            inserirFila(fila, no->esq);
+
+        if(no->dir != NULL)
+            inserirFila(fila, no->dir);
+
+    }//end while
+
+    liberaFila(fila);
+
+}//end em_largura
+
+
+// Devolve a altura da árvore binária
+// cuja raiz é r.
+
+int altura (arvore r) {
+   if (r == NULL)
+      return -1; // altura da árvore vazia
+   else {
+      int he = altura (r->esq);
+      int hd = altura (r->dir);
+      if (he < hd) return hd + 1;
+      else return he + 1;
+   }
+}
+
+int main (){
+  noh* raiz = NULL;
+  noh* ptr = NULL;
+
+  raiz = inserir (raiz, 5);
+  raiz = inserir (raiz, 3);
+  raiz = inserir (raiz, 8);
+  raiz = inserir (raiz, 1);
+  raiz = inserir (raiz, 4);
+  raiz = inserir (raiz, 6);
+  raiz = inserir (raiz, 9);
+  raiz = inserir (raiz, 0);
+  raiz = inserir (raiz, 2);
+  raiz = inserir (raiz, 7);
+
+  em_ordem (raiz);
+  int n=0;
+  count_nodes (raiz, &n);
+  printf ("=== O numero de nós na árvore: %d\n", n);
+
+  busca_folha (raiz);
+
+  busca_conteudo (raiz, 5);
+  //printf("%d\n", ptr->conteudo);
+}
---
+++ src/arvores/pilhas.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include "pilhas.h"
+
+/////////////////////////////////////
+// Inicio da definição das funções //
+/////////////////////////////////////
+
+int initia (struct stack *ps){
+  ps->top = -1;
+  return 1;
+}
+
+int empty(struct stack *ps){
+  if (ps->top == -1)
+    // Pilha vazia
+    return TRUE;
+  else
+    // Pilha não vazia
+    return FALSE;
+} // end funcao empty
+
+int pop (struct stack *ps, struct stackelement *el){
+  if (empty(ps)){
+    printf("Pilha vazia");
+    // Underflow
+    return 0;
+  } else {
+
+    *el = ps->items[ps->top];
+    (ps->top)--;
+    return 1;
+  }
+}
+
+int push_int (struct stack *ps, int x){
+  if (ps->top < STACKSIZE-1){
+    (ps->top)++;
+    ps->items[ps->top].etype = INTGR;
+    ps->items[ps->top].element.ival = x;
+    return 1;
+  } else {
+    return 0; // OVERFLOW
+  }
+}
+
+void print_elem (struct stackelement *se){
+  switch (se->etype){
+    case FLT:
+      printf("%f\n", se->element.fval);
+      break;
+    case CHR:
+      printf("%c\n", se->element.cval);
+      break;
+    case INTGR:
+      printf("%d\n", se->element.ival);
+      break;
+    default:
+      printf("Tipo desconhecido.\n");
+  }
+};
+
+int print_stack (struct stack *ps){
+  int i;
+
+  if (ps->top >= 0){
+    printf ("\n");
+    // printf("-----------------------------\n");
+    // printf("| var | cont  | end |\n");
+    printf("-----------------------------\n");
+    // printf("%d\n", (ps->top));
+    for (i=(ps->top); i>=0; i--){
+      // printf("v[%d] %x ", i, &(ps->items[i].element));
+      printf("%d. ", i+1);
+      print_elem (&(ps->items[i]));
+      // if (i==0) printf("<--- base (inicio)");
+      // if (i==ps->top) printf("<--- topo (fim)");
+      printf ("\n");
+    }
+  } else {
+    printf ("\nA pilha estah vazia!!\n");
+  }
+  printf("\n");
+  return 0;
+}
